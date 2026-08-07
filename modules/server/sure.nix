4831827510641938d77235c59535a1e8fdd92c45
Hannes Klinckaert <hannes-dev@mailbox.org>
2026-08-08 00:26:10 +0200

    fix sure

--- modules/server/sure.nix
+++ modules/server/sure.nix
@@ -79,7 +79,7 @@ in
           environment = {
             POSTGRES_USER = "sure_user";
             POSTGRES_DB = "sure_production";
-            POSTGRESS_PASSWORD = "sure_password";
+            POSTGRES_PASSWORD = "sure_password";
           };
           environmentFiles = [ config.age.secrets.${secretEnv}.path ];
           extraOptions = [ "--network=sure_net" ];
@@ -99,7 +99,7 @@ in
             DB_PORT = "5432";
             POSTGRES_USER = "sure_user";
             POSTGRES_DB = "sure_production";
-            POSTGRESS_PASSWORD = "sure_password";
+            POSTGRES_PASSWORD = "sure_password";
             REDIS_URL = "redis://sure-redis:6379/1";
             SELF_HOSTED = "true";
             RAILS_FORCE_SSL = "false";
@@ -112,7 +112,7 @@ in
             OIDC_REDIRECT_URI = "https://${cfg.domain}/auth/openid_connect/callback";
           };
           environmentFiles = [ config.age.secrets.${secretEnv}.path ];
-          ports = [ "127.0.0.1:${toString cfg.port}:3000" ];
+          ports = [ "0.0.0.0:${toString cfg.port}:3000" ];
           extraOptions = [ "--network=sure_net" ];
           volumes = [ "sure_app-storage:/rails/storage" ];
           dependsOn = [
@@ -132,7 +132,7 @@ in
             DB_HOST = "sure-db";
             DB_PORT = "5432";
             POSTGRES_USER = "sure_user";
-            POSTGRESS_PASSWORD = "sure_password";
+            POSTGRES_PASSWORD = "sure_password";
             POSTGRES_DB = "sure_production";
             REDIS_URL = "redis://sure-redis:6379/1";
             SELF_HOSTED = "true";
